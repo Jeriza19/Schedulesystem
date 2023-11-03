940fe04040351ed69952ea8e1f9c999e6420a308
2D Jeriza P. Aniñon ( P Elec 1) <135580049+Jeriza19@users.noreply.github.com>
2023-11-03 11:45:10 +0800

    Teacher.java

---
+++ Teacher.java
@@ -0,0 +1,15 @@
+class Teacher extends Person {
+    private String subject;
+
+    public Teacher() {
+        // Default constructor
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+}
