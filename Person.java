6b0c91269b4ed74ee16d09011c51308da1b34995
2D Jeriza P. Aniñon ( P Elec 1) <135580049+Jeriza19@users.noreply.github.com>
2023-11-03 11:43:02 +0800

    Person.java

---
+++ Person.java
@@ -0,0 +1,44 @@
+class Person {
+    private String name;
+    private int id;
+    private String email;
+    private int phone;
+
+    public Person() {
+        // Default constructor
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+}
+
+
